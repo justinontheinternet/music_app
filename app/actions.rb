25cc0f64ae9025ef3ec1a9dc2bd73f0d1d1480d2
JUSTIN ARRUDA <justinontheinternet@users.noreply.github.com>
2016-01-25 19:29:38 +0000

    bootstrap basics

--- app/actions.rb
+++ app/actions.rb
@@ -3,11 +3,30 @@ get '/' do
   erb :index
 end
 
-get '/songs/' do
+get '/songs' do
   @songs = Song.all
   erb :'songs/index'
 end
 
 get '/songs/new_song' do
+  @song = Song.new
   erb :'songs/new_song'
 end
+
+post '/songs' do
+  @song = Song.new(
+    title: params[:title],
+    author: params[:author],
+    url: params[:url],
+  )
+  if @song.save
+    redirect '/songs'
+  else
+    erb :'songs/new_song'
+  end
+end
+
+# get '/songs/:id' do
+#   @song = Song.find params[:id]
+#   erb :'songs/deets'
+# end
